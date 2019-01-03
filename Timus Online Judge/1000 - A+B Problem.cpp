f4c5d3adb39e7e7fd320c3bdabbb40f42df44002
Limon Safayet <limon6531@gmail.com>
2019-01-03 11:54:14 +0600

    Add files via upload

---
+++ Timus Online Judge/1000 - A+B Problem.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include<stdio.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a;
+    cin>>b;
+
+    cout<<a+b;
+}
