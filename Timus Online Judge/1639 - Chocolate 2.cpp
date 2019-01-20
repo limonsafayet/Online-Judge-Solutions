1bad4b2527945fda6d19c86fe0115cca2036ee7c
Limon Safayet <limon6531@gmail.com>
2019-01-20 15:32:09 +0600

    Add files via upload

---
+++ Timus Online Judge/1639 - Chocolate 2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int m,n;
+    cin>>m;
+    cin>>n;
+
+    if((n*m-1)%2==0)
+    {
+        cout<<"[second]=:]";
+    }
+    else
+    {
+        cout<<"[:=[first]";
+    }
+}
