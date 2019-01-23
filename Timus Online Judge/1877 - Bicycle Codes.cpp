ad2767ca57387239aefee7d61cf1fb7e9282f96f
Limon Safayet <limon6531@gmail.com>
2019-01-23 21:46:36 +0600

    Add files via upload

---
+++ Timus Online Judge/1877 - Bicycle Codes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+
+    cin>>a>>b;
+
+    if((a%2)==0 || (b%2)!=0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+}
